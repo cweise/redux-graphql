55871b954b69834b6148c9b56b6b7841466ecd49
Christoph Weise-Onnen <cwo@thepeaklab.com>
2020-03-19 15:18:54 +0100

    updated publish dir

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { REQUEST_START, REQUEST_SUCCESS, REQUEST_ERROR } from "./action-types";
 export { request } from "./actions";
 export { select, getUrl } from "./selectors";
 export { createReducer } from "./reducer";
+export { createHash } from "./utils";
